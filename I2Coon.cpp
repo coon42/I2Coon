68f724331dd1bd0a8b8c73726dd55a88a824b380
coon42 <coon@c-base.org>
2014-08-25 00:26:01 +0200

    Arduino slave receiver basicly works

--- I2Coon.cpp
+++ I2Coon.cpp
@@ -1,5 +1,42 @@
 #include "I2Coon.h"
 
+void readRegister(volatile uint8_t* pAtmegaReg, void* value) {
+  *(uint8_t*)value = *pAtmegaReg;
+}
+
+void writeRegister(volatile uint8_t* pAtmegaReg, void* value) {
+  *pAtmegaReg = *(uint8_t*)value;
+}
+
+bool enableTWI(bool enable) {
+  Reg328pPRR_t prr;
+  readRegister(&PRR, &prr);
+  prr.prtwi = enable ? 0 : 1;
+  writeRegister(&PRR, &prr);
+}
+
+bool isTWIInterruptSet() {
+  RegI2CoonTWCR_t twcr;
+  readRegister(&TWCR, &twcr);
+  return twcr.twint == 1 ? true : false;
+}
+
+void clearTWIInterrupt() {
+  RegI2CoonTWCR_t twcr;
+
+  readRegister(&TWCR, &twcr);
+  twcr.twint = 1;
+  writeRegister(&TWCR, &twcr);  
+}
+
+void clearTWIError() {
+  RegI2CoonTWCR_t twcr;
+
+  readRegister(&TWCR, &twcr);
+  twcr.twsto = 1;
+  twcr.twint = 1;
+  writeRegister(&TWCR, &twcr);
+}
 
 bool setI2CBitRate(int32_t bitRate) {
   Serial.println("setI2CBitRate()");
@@ -27,7 +64,6 @@ bool setI2CBitRate(int32_t bitRate) {
   // See Chapter 22.5.2 (page 215)
   // TODO: optimize formular?
   
-  
   TWSR = prescaler & 0x03;
   TWBR = (F_CPU / bitRate - 16) / (2 * prescaler);
   
@@ -36,4 +72,57 @@ bool setI2CBitRate(int32_t bitRate) {
   Serial.print("TWBR = "); Serial.println(TWBR);
   
   return true;
+}
+
+void setTWISlaveAddress(uint8_t slaveAddress, bool acceptGeneralCall) {
+  RegI2CoonTWAR_t twar;
+  readRegister(&TWAR, &twar);
+  twar.twa = slaveAddress;
+  twar.twgce = acceptGeneralCall ? 1 : 0;
+  writeRegister(&TWAR, &twar);
+}
+
+void initSlaveReceiverMode(uint8_t slaveAddress, bool acceptGeneralCall) {
+  setTWISlaveAddress(slaveAddress, acceptGeneralCall);
+  
+  // Enter slave mode
+  RegI2CoonTWCR_t twcr;
+  readRegister(&TWCR, &twcr);
+  twcr.twen  = 1;
+  twcr.twea  = 1;
+  twcr.twsta = 0;
+  twcr.twsto = 0;
+  writeRegister(&TWCR, &twcr);
+}
+
+uint8_t getTWIStatus() {
+  return TWSR & 0xF8;
+}
+
+uint8_t getTWISlaveAddress() {
+  RegI2CoonTWAR_t twar;
+  readRegister(&TWAR, &twar);
+  
+  return twar.twa;
+}
+
+char* debugTWIStateToString(uint8_t stateCode) {
+  switch(stateCode) {
+    
+    // State codes for slave receiver mode
+    case RECV_SLAW_ACK: return "RECV_SLAW_ACK";
+    case ARB_LOST_SLAW_RECV_ACK: return "ARB_LOST_SLAW_RECV_ACK";
+    case GEN_CALL_ADDR_RECV_ACK: return "GEN_CALL_ADDR_RECV_ACK";
+    case ARB_LOST_SLARW_GENCA_RECV_ACK: return "ARB_LOST_SLARW_GENCA_RECV_ACK";
+    case PREV_ADDR_OWN_SLAW_DATA_RECV_ACK: return "PREV_ADDR_OWN_SLAW_DATA_RECV_ACK";
+    case PREV_ADDR_OWN_SLAW_DATA_RECV_NACK: return "PREV_ADDR_OWN_SLAW_DATA_RECV_NACK";
+    case PREV_ADDR_GEN_CALL_DATA_RECV_ACK: return "PREV_ADDR_GEN_CALL_DATA_RECV_ACK";
+    case PREV_ADDR_GEN_CALL_DATA_RECV_NACK: return "PREV_ADDR_GEN_CALL_DATA_RECV_NACK";
+    case STOP_OR_RESTART_COND_RECV_W_ADS: return "STOP_OR_RESTART_COND_RECV_W_ADS";
+    
+    // misc states
+    case NO_TWI_STATE_INFO_AVAILABLE: return "NO_TWI_STATE_INFO_AVAILABLE";
+    case BUS_ERROR_ILLEGAL_START_STOP_COND: return "BUS_ERROR_ILLEGAL_START_STOP_COND";
+    default: return "unknown TWI state.";
+  }
 }
--- I2Coon.h
+++ I2Coon.h
@@ -3,7 +3,19 @@
 #ifndef I2COON_H
 #define I2COON_H
 
+void readRegister(volatile uint8_t* pReg, void* value);
+void writeRegister(volatile uint8_t* pReg, void* value);
+bool enableTWI(bool enable);
 bool setI2CBitRate(int32_t bitRate);
+uint8_t getTWIStatus();
+void setTWISlaveAddress(uint8_t slaveAddress, bool acceptGeneralCall);
+void initSlaveReceiverMode(uint8_t slaveAddress, bool acceptGeneralCall);
+uint8_t getTWISlaveAddress();
+bool isTWIInterruptSet();
+void clearTWIInterrupt();
+void clearTWIError();
+
+char* debugTWIStateToString(uint8_t stateCode);
 
 
 #define REG_TWBR  0xB8
@@ -13,39 +25,77 @@ bool setI2CBitRate(int32_t bitRate);
 #define REG_TWCR  0xBC
 #define REG_TWAMR 0xBD
 
-typedef struct {
-  uint8_t twbr;
-} RegI2CoonTWBR;
+enum TWISLRStatus_t {
+  // Status codes for slave receiver mode
+  RECV_SLAW_ACK = 0x60,                     // Own SLA+W has been received; ACK has been returned.
+  ARB_LOST_SLAW_RECV_ACK = 0x68,            // Arbitration lost in SLA+R/W as Master; own SLAüW has been received; ACK has been returned.
+  GEN_CALL_ADDR_RECV_ACK = 0x70,            // General call address has been received; ACK has been returned
+  ARB_LOST_SLARW_GENCA_RECV_ACK = 0x78,     // Arbitration lost in SLA+R/W as Master; General call address has been received; ACK has been returned.
+  PREV_ADDR_OWN_SLAW_DATA_RECV_ACK = 0x80,  // Previously addressed with own SLA+W; data has been received; ACK has been returned.
+  PREV_ADDR_OWN_SLAW_DATA_RECV_NACK = 0x88, // Previously addressed with own SLA+W; data has been received; NOT ACK has been returned.
+  PREV_ADDR_GEN_CALL_DATA_RECV_ACK = 0x90,  // Previously addressed with general call; data has been received; ACK has been returned.
+  PREV_ADDR_GEN_CALL_DATA_RECV_NACK = 0x98, // Previously addressed with general call; data has been received; NOT ACK has been returned
+  STOP_OR_RESTART_COND_RECV_W_ADS  = 0xA0, // A STOP condition or repeated START condition has been received while still addresses as Slave
+};
+
+enum TWIMiscState_t {
+  NO_TWI_STATE_INFO_AVAILABLE = 0xF8, // No relevant state information available; TWINT = "0".
+  BUS_ERROR_ILLEGAL_START_STOP_COND = 0x00, // Bus error due to an illegal START or STOP condition.
+};
 
 typedef struct {
-  uint8_t twie  : 1;
-  uint8_t       : 1;
-  uint8_t twen  : 1;
-  uint8_t twwc  : 1;
-  uint8_t twsto : 1;
-  uint8_t twsta : 1;
-  uint8_t twea  : 1;
-  uint8_t twint : 1;
-} RegI2CoonTWCR;
+  uint8_t 
+  twbr;
+} RegI2CoonTWBR_t;
 
 typedef struct {
-  uint8_t twps : 2;
-  uint8_t      : 1;
-  uint8_t tws  : 5;
-} RegI2CoonTWSR;
+  uint8_t 
+  twie  :1,
+        :1,
+  twen  :1,
+  twwc  :1,
+  twsto :1,
+  twsta :1,
+  twea  :1,
+  twint :1;
+} RegI2CoonTWCR_t;
 
 typedef struct {
-  uint8_t twd;
-} RegI2CoonTWDR;
+  uint8_t
+  twps :2,
+       :1,
+  tws  :5;
+} RegI2CoonTWSR_t;
 
 typedef struct {
-  uint8_t twgce : 1;
-  uint8_t twa   : 7;
-} RegI2CoonTWAR;
+  uint8_t 
+  twd;
+} RegI2CoonTWDR_t;
 
 typedef struct {
-  uint8_t      : 1;
-  uint8_t twam : 7;
-} RegI2CoonTWAMR;
+  uint8_t 
+  twgce :1,
+  twa   :7;
+} RegI2CoonTWAR_t;
+
+typedef struct {
+  uint8_t
+       : 1,
+  twam : 7;
+} RegI2CoonTWAMR_t;
+
+
+// 328p registers
+typedef struct {
+  uint8_t
+  pradc    :1,
+  prusart0 :1,
+  prspi    :1,
+  prtim1   :1,
+           :1,
+  prtim0   :1,
+  prtim2   :1,
+  prtwi    :1;
+} Reg328pPRR_t;
 
 #endif /* I2COON_H */
